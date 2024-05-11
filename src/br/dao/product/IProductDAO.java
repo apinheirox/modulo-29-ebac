757b68eec45cdf1bff2bccd2ba93b5694d9e5512
Antônio Pinheiro <techwoodev@gmail.com>
2024-05-10 21:10:41 -0300

    .

---
+++ src/br/dao/product/IProductDAO.java
@@ -0,0 +1,15 @@
+package br.dao.product;
+
+import br.domain.Product;
+
+import java.util.List;
+
+public interface IProductDAO {
+
+    public Integer toAdd(Product product) throws Exception;
+    public Integer toRefresh(Product product) throws Exception;
+    public Product toFind(Long id) throws Exception;
+
+    public List<Product> allToFind() throws Exception;
+    public Integer delete(Product product) throws Exception;
+}
